c6311d1b1095282ae4deb03787ced74a0cdb0e69
Coceus <46695012+Coceus@users.noreply.github.com>
2020-04-12 16:51:02 -0400

    fix for bpb

--- main.c
+++ main.c
@@ -8,7 +8,13 @@ int main(int argc, char *argv[]) {
 	char command[100];
 	char exit[100];
 	strcpy(exit, "exit");
-	unsigned char buffer[100];
+	unsigned char bpb[100];
+	unsigned char bpb1[100];
+	unsigned char bpb2[100];
+	unsigned char bpb3[100];
+	unsigned char bpb4[100];
+	unsigned char bpb5[100];
+	unsigned char bpb6[100];
 
 
 
@@ -22,32 +28,33 @@ int main(int argc, char *argv[]) {
 
 		if (strcmp(command, "info") == 0) {
 			lseek(fileNumber, 11, SEEK_SET);
-			read(fileNumber, buffer, 2);
-			capture = *(int*)buffer;
+			read(fileNumber, bpb, 2);
+			capture = *(int*)bpb;
 			printf("Bytes per sec: %d\n", capture);
 			lseek(fileNumber, 13, SEEK_SET);
-			read(fileNumber, buffer, 1);
-			capture = *(int*)buffer;
+
+			read(fileNumber, bpb1, 1);
+			capture = *(int*)bpb1;
 			printf("Sector per Cluser: %d\n", capture);
 			lseek(fileNumber, 14, SEEK_SET);
-			read(fileNumber, buffer, 2);
-			capture = *(int*)buffer;
+			read(fileNumber, bpb2, 2);
+			capture = *(int*)bpb2;
 			printf("reseverd sector count: %d\n", capture);
 			lseek(fileNumber, 16, SEEK_SET);
-			read(fileNumber, buffer, 1);
-			capture = *(int*)buffer;
+			read(fileNumber, bpb3, 1);
+			capture = *(int*)bpb3;
 			printf("number of FATs: %d\n", capture);
 			lseek(fileNumber, 32, SEEK_SET);
-			read(fileNumber, buffer, 4);
-			capture = *(int*)buffer;
+			read(fileNumber, bpb4, 4);
+			capture = *(int*)bpb4;
 			printf("total sectors %d\n", capture);
 			lseek(fileNumber, 36, SEEK_SET);
-			read(fileNumber, buffer, 4);
-			capture = *(int*)buffer;
+			read(fileNumber, bpb5, 4);
+			capture = *(int*)bpb5;
 			printf("FATsize: %d\n", capture);
 			lseek(fileNumber, 44, SEEK_SET);
-			read(fileNumber, buffer, 4);
-			capture = *(int*)buffer;
+			read(fileNumber, bpb6, 4);
+			capture = *(int*)bpb6;
 			printf("FAT Root Cluster: %d\n", capture);
 			//printf("%d", *i);
 
